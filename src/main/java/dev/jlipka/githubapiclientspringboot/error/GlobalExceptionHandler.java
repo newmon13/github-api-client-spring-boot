d6934ab6317fb409ea9dd14191c6f76349c172aa
Moher <47338928+newmon13@users.noreply.github.com>
2025-03-30 22:59:28 +0200

    Added missing 404 http status in ErrorResponse

--- src/main/java/dev/jlipka/githubapiclientspringboot/error/GlobalExceptionHandler.java
+++ src/main/java/dev/jlipka/githubapiclientspringboot/error/GlobalExceptionHandler.java
@@ -2,13 +2,15 @@ package dev.jlipka.githubapiclientspringboot.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ErrorResponse handleUserNotFoundException() {
-        return new ErrorResponse(HttpStatus.NOT_FOUND, "User does not exist");
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleUserNotFoundException(UserNotFoundException ex) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 }
