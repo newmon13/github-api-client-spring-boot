c7ecad1b715c62c9ad7688ae88a9b13ae9ca55b4
Moher <47338928+newmon13@users.noreply.github.com>
2025-03-30 11:57:40 +0200

    Initial commit

---
+++ src/main/java/dev/jlipka/githubapiclientspringboot/GithubApiClientSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.jlipka.githubapiclientspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiClientSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiClientSpringBootApplication.class, args);
+    }
+
+}
